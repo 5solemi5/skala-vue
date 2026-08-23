7cb57793cd964bacb29a4573872f816b436c44e8
5solemi5 <seizeju@gmail.com>
2026-08-23 14:36:48 +0900

    Code Challenge 2: Vue Directive (p.93)
    - v-html / v-html XSS / v-text : VueHtml, VueHtmlXss, VueText
    - v-bind (기본 / 클래스 / 스타일 / 단축문법) : VueBind, VueBindClass, VueBindStyle, VueBindShorthand
    - v-if / v-else-if / v-else / v-show : VueIf, VueShow
    - v-for (배열 / 객체 / 배열 내 객체) : VueFor
    - v-pre / v-cloak / v-once / v-memo : VuePre, VueCloak, VueOnce, VueMemo
    - App.vue 에 2번 섹션 추가 (3~5번 블록은 진도에 맞춰 주석 해제 예정)

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,35 @@
 // 1. 학습환경 구성
 import SampleOne from './components/practices/basic/SampleOne.vue'
 import SampleTwo from './components/practices/basic/SampleTwo.vue'
+
+// 2. Vue Directive
+import VueHtml from './components/practices/basic/VueHtml.vue'
+import VueHtmlXss from './components/practices/basic/VueHtmlXss.vue'
+import VueText from './components/practices/basic/VueText.vue'
+import VueBind from './components/practices/basic/VueBind.vue'
+import VueBindClass from './components/practices/basic/VueBindClass.vue'
+import VueBindStyle from './components/practices/basic/VueBindStyle.vue'
+import VueBindShorthand from './components/practices/basic/VueBindShorthand.vue'
+import VueIf from './components/practices/basic/VueIf.vue'
+import VueShow from './components/practices/basic/VueShow.vue'
+import VueFor from './components/practices/basic/VueFor.vue'
+import VuePre from './components/practices/basic/VuePre.vue'
+import VueCloak from './components/practices/basic/VueCloak.vue'
+import VueOnce from './components/practices/basic/VueOnce.vue'
+import VueMemo from './components/practices/basic/VueMemo.vue'
+
+// 3. Vue Event Handling
+// import EventBasic from './components/practices/basic/EventBasic.vue'
+// import EventObject from './components/practices/basic/EventObject.vue'
+// import EventModifier from './components/practices/basic/EventModifier.vue'
+
+// 4. Vue Form Handling
+// import ModelBasic from './components/practices/basic/ModelBasic.vue'
+// import ModelForm from './components/practices/basic/ModelForm.vue'
+// import ModelModifier from './components/practices/basic/ModelModifier.vue'
+
+// 5. Vue Style Handling
+// import StyleScoped from './components/practices/basic/StyleScoped.vue'
 </script>
 
 <template>
@@ -10,6 +39,40 @@ import SampleTwo from './components/practices/basic/SampleTwo.vue'
     <hr />
     <SampleOne />
     <SampleTwo />
+
+    <h1>2. Vue Directive</h1>
+    <hr />
+    <VueHtml />
+    <VueHtmlXss />
+    <VueText />
+    <VueBind />
+    <VueBindClass />
+    <VueBindStyle />
+    <VueBindShorthand />
+    <VueIf />
+    <VueShow />
+    <VueFor />
+    <VuePre />
+    <VueCloak />
+    <VueOnce />
+    <VueMemo />
+
+    <!-- 진도 나갈 때 해당 블록의 import와 태그 주석을 함께 해제한다 -->
+    <!--
+    <h1>3. Vue Event Handling</h1>
+    <hr />
+    <EventBasic />
+    <EventObject />
+    <EventModifier />
+    <h1>4. Vue Form Handling</h1>
+    <hr />
+    <ModelBasic />
+    <ModelForm />
+    <ModelModifier />
+    <h1>5. Vue Style Handling</h1>
+    <hr />
+    <StyleScoped />
+    -->
   </div>
 </template>
 
